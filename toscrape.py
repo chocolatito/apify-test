98977a1c06e2f61d24ec36398666b78f2bd33401
chocolatito <dannny.mx@gmail.com>
2024-10-22 10:36:56 -0300

    test

--- toscrape.py
+++ toscrape.py
@@ -11,6 +11,11 @@ import apify
 
 async def main():
     apify.utils.log.info("START CRAWLER")
+    input_path = os.getenv('APIFY_INPUT_KEY', 'INPUT.json')
+    with open(input_path, 'r') as input_file:
+        input_data = json.load(input_file)
+    url = input_data.get('url', 'http://quotes.toscrape.com')
+    apify.utils.log.info(f'req to: "{url}"')
     xp_top_ten_tags = '//h2[contains(text(),"Top")]/..//a[contains(@href,"/tag/")]'
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -19,23 +24,23 @@ async def main():
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
     }
-    BASE_URL = 'http://quotes.toscrape.com'
-    res = requests.get(BASE_URL, headers=headers, verify=False)
+    res = requests.get(url, headers=headers, verify=False)
     tree = html.fromstring(res.text)
     els = tree.xpath(xp_top_ten_tags)
     assert els
     apify.utils.log.info(f'Total tags: {len(els)}')
-    tag_dict = {}
+    results = {}
     for el in els:
         tag = el.text
         href = el.attrib['href']
         apify.utils.log.info(f'Tag "{tag}"')
-        tag_dict[tag] = BASE_URL+href
-
-    apify.utils.log.info(f"tag_dict = {json.dumps(tag_dict)}")
-    await apify.push_data(tag_dict)
+        results[tag] = url+href
+    apify.utils.log.info(f"results = {json.dumps(results)}")
+    with open('OUTPUT.json', 'w') as output_file:
+        json.dump(results, output_file)
+    await apify.push_data(results)    
     apify.utils.log.info("END CRAWLER")
 
-apify.main(main)
 
-
+if __name__ == '__main__':
+    main()
